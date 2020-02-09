90f1377d80bcd973b46f785c8c9e85d60f41bc0f
jgamezl <jgamez@moddtech.com>
2020-02-09 16:22:47 -0700

    route2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host: 'localhost',
     user : 'root',
-    password : '',
+    password : 'guitarra5',
     database : 'klimadaten'
 });
 
